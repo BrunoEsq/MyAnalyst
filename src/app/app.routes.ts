ee8918f327e3ca06de84dec890e5ea1d57ca98ea
Bruno Esquiva <brunoesquiva1211@gmail.com>
2024-09-11 13:38:02 +0200

    Medio Hecho

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { CreateFormComponent } from './create-form/create-form.component';
 import { ChartsComponent } from './charts/charts.component';
 import { LineChartComponent } from './line-chart/line-chart.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 export const routes: Routes = [
-    { path: 'charts', component: CreateFormComponent },
-    { path: '', component: LineChartComponent },
+    { path: 'create', component: CreateFormComponent },
+    { path: '', component: HomepageComponent },
+    { path: 'charts', component: LineChartComponent },
 
 ];
