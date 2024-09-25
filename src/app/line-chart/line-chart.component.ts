a84014ab8431d255508c737b010dcc78ac670c51
Bruno Esquiva <brunoesquiva1211@gmail.com>
2024-09-25 18:41:27 +0200

    Tres nuevos charts incorporados

--- src/app/line-chart/line-chart.component.ts
+++ src/app/line-chart/line-chart.component.ts
@@ -90,22 +90,54 @@ export class LineChartComponent implements OnInit {
         type: 'bar',
         data: datas,
       });
-    } else {
+    } else if (this.t_chart == "3") {
+      // Mejorar y poder incluir varios labels
       datas = {
         labels: this.labels_array,
         datasets: [{
           label: this.name,
           data: this.data_array,
-          fill: false,
+          fill: true,
           borderColor: this.color.toLowerCase(),
+          backgroundColor: this.color.toLowerCase(),
           tension: 0.1
-
         }]
+      };
 
-      }
+      this.chart = new Chart("chart", {
+        type: 'line',
+        data: datas,
+      });
+
+      // }else if (this.t_chart == "4") {
+
+      //   datas = {
+      //     labels: [
+      //       'Red',
+      //       'Blue',
+      //       'Yellow'
+      //     ],
+      //     datasets: [{
+      //       label: 'My First Dataset',
+      //       data: [300, 50, 100],
+      //       backgroundColor: [
+      //         'rgb(255, 99, 132)',
+      //         'rgb(54, 162, 235)',
+      //         'rgb(255, 205, 86)'
+      //       ],
+      //       hoverOffset: 4
+      //     }]
+      //   };
+
+      //   this.chart = new Chart("chart", {
+      //     type: 'doughnut',  
+      //     data: datas,
+      //   });
+      // }
+
+
     }
-
-
-
   }
+
+
 }
